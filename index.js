387ce37e36b165799e3fad6e1cdd29dd41611c15
Alejandro Vasquez <alejandrovasquez@Alejandros-iMac.local>
2021-04-14 22:25:18 -0700

    adding more changes

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ function copy(array){
 }    
 
 // copy(originalFlavors)
+// New Push
 
 
 
@@ -198,7 +199,7 @@ Use the getAverageWordLength function below to do the following:
     For example: getAverageWordLength(originalFlavors) should return a number between 0 and 3.     
 */
 
-function getAverageWordLength(/*code here*/){
+function getAverageWordLength(array){
     /*code here*/
 }
 
